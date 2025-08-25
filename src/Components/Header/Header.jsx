1f1c492bfcca3de2a135237c4388228f0ea6e422
sohaibs-liberary <muhammadsohaibshakeel8gmail.com>
2025-08-25 16:35:23 +0500

    set up an project

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logoimg from ./assets/images/download.png;
+
+function Header() {
+
+
+
+    return (
+        <>
+        <a href="/">
+        <img src={LOgoimg} alt="" /></a>
+
+        </>
+    );
+}
+
+export default Header
