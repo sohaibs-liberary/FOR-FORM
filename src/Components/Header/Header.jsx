dd4c07eed0ab39085854eab40aa46dbfca71f9e9
sohaibs-liberary <muhammadsohaibshakeel8gmail.com>
2025-08-26 14:19:21 +0500

    navbar completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { CiSearch } from "react-icons/ci";
 import Logoimg from "/src/assets/images/download.png";
-import { CgProfile } from "react-icons/cg";
+import { CiUser } from "react-icons/ci";
+import { IoBagRemoveOutline } from "react-icons/io5";
 
 const Mainmenu = [
   {
@@ -55,8 +56,10 @@ function  Header() {
           </ul>
         </div>
         <span className="flex gap-7 " >
-         <CiSearch  className=" text-3xl font-bold curser-pointer "/>
-          <CgProfile className=" text-3xl font-bold" />
+         <a><CiSearch  className=" text-3xl font-bold curser-pointer"/> </a>
+          <CiUser className=" text-3xl font-bold" />
+          <IoBagRemoveOutline className=" text-3xl font-bold" />
+          
 
         </span>
         
